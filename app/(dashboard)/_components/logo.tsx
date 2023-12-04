8a8378749d982d6c5f9d3ff7a422ab40e01398ba
Prashant Bhardwaj <geekyprashant840@gmail.com>
2023-12-04 16:24:05 +0530

    Completed Course Creation UI

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -2,14 +2,14 @@ import Image from "next/image"
 
 export const Logo = () => {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-[2]">
       <Image
         height={50}
         width={50}
         alt="logo"
         src="/logo.svg"
       />
-      <span className="font-semibold text-xl">365LMS</span>
+      <span className="font-bold text-xl text-[#0369a1]">365LMS</span>
     </div>
   )
 }
