a7879d91e8c6cf4d32347d48333be50fbcb89325
Prashant Bhardwaj <geekyprashant840@gmail.com>
2023-12-15 21:48:03 +0530

    Completed Course Category Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { LayoutDashboard } from "lucide-react";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -18,7 +19,15 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     where: {
       id: params.courseId
     }
+  });
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc"
+    }
   })
+
+  console.log(categories);
 
   if (!course) {
     return redirect("/");
@@ -68,6 +77,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           <ImageForm
             initialData={course}
             courseId={course.id}
+          />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
           />
         </div>
       </div>
