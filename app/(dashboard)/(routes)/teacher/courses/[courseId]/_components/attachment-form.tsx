3678993b1d5acbdce4400aa704af76e388cee146
Prashant Bhardwaj <geekyprashant840@gmail.com>
2023-12-18 22:44:55 +0530

    Completed Attachment API

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -7,9 +7,8 @@ import { toast } from 'react-hot-toast';
 import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button"
-import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
+import { PlusCircle } from "lucide-react";
 import { Attachment, Course } from "@prisma/client";
-import Image from "next/image";
 import { FileUpload } from './../../../../../../../components/file-upload';
 
 interface AttachmentFormProps {
