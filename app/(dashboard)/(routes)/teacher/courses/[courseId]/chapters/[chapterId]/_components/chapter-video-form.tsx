1feb3a8f706c6d159d53962e438f72b9c252b34a
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-01-06 13:09:27 +0530

    Fixing Mux Video Player Issue

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -3,7 +3,6 @@
 import * as z from "zod";
 import axios from "axios";
 import MuxPlayer from "@mux/mux-player-react";
-import Hls from 'hls.js';
 import { useState } from "react";
 import { toast } from 'react-hot-toast';
 import { useRouter } from "next/navigation";
--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,6 @@
 import Mux from "@mux/mux-node";
-import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
 
@@ -9,13 +9,12 @@ const { Video } = new Mux(
   process.env.MUX_TOKEN_SECRET!
 );
 
-export async function PATCH(
+export async function DELETE(
   req: Request,
   { params }: { params: { courseId: string; chapterId: string } }
 ) {
   try {
     const { userId } = auth();
-    const { isPublished, ...values } = await req.json();
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
@@ -32,17 +31,18 @@ export async function PATCH(
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    const chapter = await db.chapter.update({
+    const chapter = await db.chapter.findUnique({
       where: {
         id: params.chapterId,
         courseId: params.courseId,
       },
-      data: {
-        ...values,
-      },
     });
 
-    if (values.videoUrl) {
+    if (!chapter) {
+      return new NextResponse("Not Found", { status: 404 });
+    }
+
+    if (chapter.videoUrl) {
       const existingMuxData = await db.muxData.findFirst({
         where: {
           chapterId: params.chapterId,
@@ -57,25 +57,35 @@ export async function PATCH(
           },
         });
       }
+    }
 
-      const asset = await Video.Assets.create({
-        input: values.videoUrl,
-        playback_policy: "public",
-        test: false,
-      });
+    const deletedChapter = await db.chapter.delete({
+      where: {
+        id: params.chapterId,
+      },
+    });
 
-      await db.muxData.create({
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    if (!publishedChaptersInCourse.length) {
+      await db.course.update({
+        where: {
+          id: params.courseId,
+        },
         data: {
-          chapterId: params.chapterId,
-          assetId: asset.id,
-          playbackId: asset.playback_ids?.[0]?.id,
+          isPublished: false,
         },
       });
     }
 
-    return NextResponse.json(chapter);
+    return NextResponse.json(deletedChapter);
   } catch (error) {
-    console.log("[COURSES_CHAPTER_ID]", error);
+    console.log("[CHAPTER_ID_DELETE]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
